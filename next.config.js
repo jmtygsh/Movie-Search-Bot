029e0acb7b7d92c543c883ec85bb5b5e7b78f423
Yogesh Jamatia <jmtygsh@gmail.com>
2023-04-18 22:54:04 +0530

    update next.config

--- next.config.js
+++ next.config.js
@@ -2,23 +2,6 @@ const nextConfig = {
   reactStrictMode: true,
   images: {
     domains: ['cdn.tuk.dev'],
-  },
-  async redirects() {
-    return [
-      {
-        source: '/:path*',
-        has: [
-          {
-            type: 'host',
-            value: 'www.dumb-bots.online',
-            startsWith: true,
-            not: false
-          }
-        ],
-        destination: 'https://dumb-bots.online/:path*',
-        permanent: true,
-      },
-    ]
   }
 }
 module.exports = nextConfig;
