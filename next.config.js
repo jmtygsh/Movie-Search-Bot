646831dc9fb5913d14859d632c7d9be4b934ea93
Yogesh Jamatia <jmtygsh@gmail.com>
2023-04-18 21:26:37 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,6 +3,23 @@ const nextConfig = {
   images: {
     domains: ['cdn.tuk.dev'],
   },
+  async rewrites() {
+    return [
+      {
+        source: '/:path*',
+        destination: 'https://dumb-bots.online/:path*',
+        has: [
+          {
+            type: 'host',
+            value: 'www.dumb-bots.online',
+            startsWith: true,
+            not: false
+          }
+        ],
+        permanent: true
+      }
+    ]
+  }
 }
 
-module.exports = nextConfig
+module.exports = nextConfig;
