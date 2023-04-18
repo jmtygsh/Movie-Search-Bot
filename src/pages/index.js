de298fe6a0ef75cdbfca559a073eb8a6229d1af5
Yogesh Jamatia <jmtygsh@gmail.com>
2023-04-18 14:37:43 +0530

    update analytics

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,12 +11,15 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
 
         <script async src="https://www.googletagmanager.com/gtag/js?id=G-3072T6W158"></script>
-        <script>
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments)}
-          gtag('js', new Date());
-
-          gtag('config', 'G-3072T6W158');
+        <script dangerouslySetInnerHTML={
+          {
+            __html: `
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+                    gtag('config', 'G-3072T6W158');
+         `}
+        }>
         </script>
 
       </Head>
