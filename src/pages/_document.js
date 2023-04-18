c2f40d483f9d6809d81c2f416c0f78a91f4c7987
Yogesh Jamatia <jmtygsh@gmail.com>
2023-04-18 14:01:28 +0530

    add analytics

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,10 +1,18 @@
 import { Html, Head, Main, NextScript } from 'next/document'
-
+import Script from 'next/script'
 export default function Document() {
   return (
     <Html lang="en">
       <Head>
         <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet' />
+        <Script src="https://www.googletagmanager.com/gtag/js?id=G-3072T6W158"></Script>
+        <Script>
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments)}
+          gtag('js', new Date());
+
+          gtag('config', 'G-3072T6W158');
+        </Script>
       </Head>
       <body>
         <Main />
