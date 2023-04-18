aa0afd78413c5b8943ffa172dcd6e5bd4e5c29a9
Yogesh Jamatia <jmtygsh@gmail.com>
2023-04-18 09:30:26 +0530

    Update FAQ

--- src/pages/Index/Faq.js
+++ src/pages/Index/Faq.js
@@ -26,41 +26,41 @@ const Faq = () => {
 
                 <div className='text-left border-solid border-2 p-5 lg:p-10 rounded-lg mt-7'>
                     <h2 className='text-[18px] lg:text-xl mb-2'><i className='bx bx-file'></i>How to search anime on <span className='lg:pl-0 pl-3'>dumb-bots?</span></h2>
-                    <p className=' ml-3 text-[15px]'>Dumb-bots are search bots designed to help you find information. 
-                    However, at the moment, we do not have a dedicated anime search bots, 
-                    which means that some anime titles may not be available.
-                     But we have good news, we are working on adding this feature 
-                     to provide you with a better experience. In the meantime, 
-                     we kindly ask for your patience.</p>
+                    <p className=' ml-3 text-[15px]'>Dumb-bots are search bots designed to help you find information.
+                        However, at the moment, we do not have a dedicated anime search bots,
+                        which means that some anime titles may not be available.
+                        But we have good news, we are working on adding this feature
+                        to provide you with a better experience. In the meantime,
+                        we kindly ask for your patience.</p>
                 </div>
 
                 <div className='text-left border-solid border-2 p-5 lg:p-10 rounded-lg mt-7'>
                     <h2 className='text-[18px] lg:text-xl mb-2'><i className='bx bx-file'></i>How to buy dumb-bots <span className='lg:pl-0 pl-3'>script?</span></h2>
-                    <p className=' ml-3 text-[15px]'>We&apos;re excited to announce that you can now purchase dumb-bots 
-                    through our pricing page. If you encounter any issues with the &apos;Buy Now&apos;
-                     button, please don&apos;t hesitate to contact us at devygsh@gmail.com. 
-                     We also offer &apos;Fully control&apos;, &apos;Bug Fix Updates&apos;, 
-                    so, what are you wating for buy it now.</p>
+                    <p className=' ml-3 text-[15px]'>We&apos;re excited to announce that you can now purchase dumb-bots
+                        through our pricing page. If you encounter any issues with the &apos;Buy Now&apos;
+                        button, please don&apos;t hesitate to contact us at devygsh@gmail.com.
+                        We also offer &apos;Fully control&apos;, &apos;Bug Fix Updates&apos;,
+                        so, what are you wating for buy it now.</p>
                 </div>
 
                 <div className='text-left border-solid border-2 p-5 lg:p-10 rounded-lg mt-7'>
                     <h2 className='text-[18px] lg:text-xl mb-2'><i className='bx bx-file'></i>How to provide contribution <span className='lg:pl-0 pl-3'>to dumb-bots?</span></h2>
-                    <p className=' ml-3 text-[15px]'>If you encounter any issues with dumb-bots search result, 
-                    please don&apos;t hesitate to contact us through our 
-                    &apos;Contact Us&apos; page. We value your feedback, 
-                    so please feel free to share any tips or suggestions 
-                    for additional features that you would like to 
-                    see in dumb-bots. <u className='text-red-500'>So let&apos;s make dumb-bots free for all and easy to use together</u>,
-                    Thank you for your support.</p>
+                    <p className=' ml-3 text-[15px]'>If you encounter any issues with dumb-bots search result,
+                        please don&apos;t hesitate to contact us through our
+                        &apos;Contact Us&apos; page. We value your feedback,
+                        so please feel free to share any tips or suggestions
+                        for additional features that you would like to
+                        see in dumb-bots. <u className='text-red-500'>So let&apos;s make dumb-bots free for all and easy to use together</u>,
+                        Thank you for your support.</p>
                 </div>
 
                 <div className='text-left border-solid border-2 p-5 lg:p-10 rounded-lg  mt-7 mb-24'>
                     <h2 className='text-[18px] lg:text-xl mb-2'><i className='bx bx-file'></i>How to download from <span className='lg:pl-0 pl-3'>dumb-bots?</span></h2>
-                    <p className=' ml-3 text-[15px]'>Our bots only bring the data from various websites that you are looking to for, but the download process is handled by third-party websites.
+                    <p className=' ml-3 text-[15px]'>Our bots only bring the data from various websites that you are looking for, but the download process is handled by third-party websites beacuse we don't host or store any files, Our bots works real time.
                     </p>
                 </div>
 
-                
+
             </div>
         </div>
     )
