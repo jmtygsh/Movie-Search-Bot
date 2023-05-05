9c998675a8c084553d12c1b5381ba7e52435420f
Yogesh Jamatia <jmtygsh@gmail.com>
2023-05-05 12:53:46 +0530

    Update DMCA

--- src/pages/Index/Protection.js
+++ src/pages/Index/Protection.js
@@ -10,7 +10,7 @@ const Protection = () => {
 
             <Script src="https://images.dmca.com/Badges/DMCABadgeHelper.min.js"></Script>
 
-            <a href="//www.dmca.com/Protection/Status.aspx?ID=b4ae0429-1376-4d8b-a227-8546c3a01d8b" target='_blank' title="DMCA.com Protection Status" className="dmca-badge">
+            <a href="https://www.dmca.com/r/w5jy3p3" target='_blank' title="DMCA.com Protection Status" className="dmca-badge">
                 <Image src="https://images.dmca.com/Badges/dmca_protected_sml_120n.png?ID=b4ae0429-1376-4d8b-a227-8546c3a01d8b" alt="DMCA.com Protection Status" width={100} height={100} />
             </a>
         </>
